1c8058e04ce3e6aafe972c6ac3710a1f137f3626
Catherine Bronzo <cbronzo@mac.com>
2018-10-03 01:14:57 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,6 +26,16 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    
+    
+    
+    
+    if Preference.last.allow_create_songs == true
+      @song = Song.new
+    else
+      redirect_to songs_path
+    end
+    
   end
 
   def create
